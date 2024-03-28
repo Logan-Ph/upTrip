7f8cb81869d055b2ed9a60a39a07db9eb505610a
Logan <phamphuocsang5850@gmail.com>
2024-03-28 21:44:17 +0700

    add navigation to PageNotFound page

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,87 +1,83 @@
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
-import Header from './Header'
-import Footer from './Footer'
-import Homepage from '../pages/Homepage'
-import Login from '../pages/Login'
-import Admin from '../pages/Admin'
-import Unauthorized from '../pages/Unauthorized'
-import PersistAndRequireAuth from './PersistAndRequireAuth'
-import VerifyEmail from '../pages/VerifyEmail'
-import PageNotFound from '../pages/PageNotFound'
-import { ToastContainer } from 'react-toastify'
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Homepage from "../pages/Homepage";
+import Login from "../pages/Login";
+import Admin from "../pages/Admin";
+import Unauthorized from "../pages/Unauthorized";
+import PersistAndRequireAuth from "./PersistAndRequireAuth";
+import VerifyEmail from "../pages/VerifyEmail";
+import PageNotFound from "../pages/PageNotFound";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SignUp from "../pages/SignUp";
 
-
 export default function Router() {
-    const UserLayout = ({ header, footer }) => {
-        return (
-            <>
-                <ToastContainer
-                    position="top-center"
-                    autoClose={5000}
-                    hideProgressBar={true}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover={false}
-                    theme="light"
-                    stacked
-                />
-                {header}
-                <Outlet />
-                {footer}
-            </>
-        )
-    }
-
-    const BrowserRoutes = createBrowserRouter([
-      {
-        path: "/",
-        element: <UserLayout header={<Header />} footer={<Footer />} />,
-        children: [
-          {
-            path: "/",
-            element: <Homepage />,
-          },
-          {
-            path: "/unauthorized",
-            element: <Unauthorized />,
-          },
-          {
-            path: "*",
-            element: <PageNotFound />,
-          },
-          {
-            path: "/verify-email",
-            element: <VerifyEmail />,
-          },
-          {
-            path: "/",
-            element: <PersistAndRequireAuth />,
-            children: [
-              {
-                path: "/admin",
-                element: <Admin />,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: "/signup",
-        element: <SignUp />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-    ]);
-
+  const UserLayout = ({ header, footer }) => {
     return (
-        <RouterProvider router={BrowserRoutes} />
-    )
-}
+      <>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover={false}
+          theme="light"
+          stacked
+        />
+        {header}
+        <Outlet />
+        {footer}
+      </>
+    );
+  };
 
+  	const BrowserRoutes = createBrowserRouter([
+    {
+		path: "/",
+		element: <UserLayout header={<Header />} footer={<Footer />} />,
+		children: [
+			{
+				path: "/",
+				element: <Homepage />,
+			},
+			{
+				path: "/unauthorized",
+				element: <Unauthorized />,
+			},
+			{
+				path: "/verify-email",
+				element: <VerifyEmail />,
+			},
+			{
+				path: "/",
+				element: <PersistAndRequireAuth />,
+				children: [
+					{
+					path: "/admin",
+					element: <Admin />,
+					},
+				],
+			},
+      	],
+    },
+    {
+		path: "/signup",
+		element: <SignUp />,
+    },
+    {
+		path: "/login",
+		element: <Login />,
+    },
+    {
+		path: "*",
+		element: <PageNotFound />,
+    },
+  	]);
+
+  return <RouterProvider router={BrowserRoutes} />;
+}
