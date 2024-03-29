a0060a3c86b69e006f3bb3a76198e5fdbd58891a
Logan-Ph <phamphuocsang5850@gmail.com>
2024-03-29 10:25:37 +0700

    change the root node of the react-toastify

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,28 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId='253190647879-4t51i19p4igdkvtmu2o3g20dkochpiem.apps.googleusercontent.com'>
       <AuthProvider>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          stacked
+          pauseOnHover={false}
+          theme="light"
+        />
         <App />
       </AuthProvider>
     </GoogleOAuthProvider>
