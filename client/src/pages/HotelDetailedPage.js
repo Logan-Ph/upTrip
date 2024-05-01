b347ce1b4b322288d0a40971f79ebd47978f4006
nguyenngocthanhmai <mainguyenngocthanh040426@gmail.com>
2024-05-01 19:07:04 +0700

    gallery

--- client/src/pages/HotelDetailedPage.js
+++ client/src/pages/HotelDetailedPage.js
@@ -1,9 +1,11 @@
+import DetailPageGallery from "../components/DetailPageGallery";
+
 export default function HotelDetailedPage(){
     return(
         <>
         <div className="bg-[#FAFBFC] md:p-10">
                 <section className="mx-auto max-w-8xl px-6 py-6">
-                    
+                    <DetailPageGallery/>
                 </section>
         </div>
         </>
