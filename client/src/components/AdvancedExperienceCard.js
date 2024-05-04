7280e7403b6041b8e9467665e1bb815e3e4a6c78
nguyenngocthanhmai <mainguyenngocthanh040426@gmail.com>
2024-05-04 20:04:31 +0700

    HEART UPDATE

--- client/src/components/AdvancedExperienceCard.js
+++ client/src/components/AdvancedExperienceCard.js
@@ -4,12 +4,9 @@ export function AdvancedExperienceCard({ data }) {
 
     return (
         <>
-            <div>
-                
-            <ExperienceCard data={data}/>
-                
+            <div> 
+                <ExperienceCard data={data}/>
             </div>
-            
         </>
     );
 }
@@ -56,17 +53,17 @@ function ExperienceCard({data}){
         ? "opacity-100 translate-y-10 transition ease-in-out"
         : "opacity-0 translate-y-10";
 
-        const [isFilled, setIsFilled] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
 
-        const toggleHeart = () => {
-            setIsFilled(!isFilled);
-        };
+    const toggleHeart = () => {
+        setIsFilled(!isFilled);
+    };
 
 
     return (
         <>
-            <div ref={ref}
-         className={`border border-[#CDEAE1] rounded-lg transition ease-in-out delay-100  hover:translate-y-0.5 hover:translate-x-0.5 hover:scale-100 duration-300 shadow-md ${visibilityClass}`}>
+        <div ref={ref}
+         className={`relative border border-[#CDEAE1] rounded-lg transition ease-in-out delay-100  hover:translate-x-1 hover:scale-105 duration-300 shadow-md ${visibilityClass}`}>
                 <div>
                     <img src={data.card.coverImageUrl}
                         className="w-full h-[245px] object-cover rounded-lg"/>
@@ -133,23 +130,22 @@ function ExperienceCard({data}){
                     {/* Followed Agoda */}
                     <div className="bg-[#fac800] px-4 py-1">
                         <p className="font-bold text-black">Top tour</p>
-                    </div>
-
-                    <div className="border border-[#8DD3BB] w-[30px] h-[30px] flex items-center justify-between rounded-md cursor-pointer py-2" onClick={toggleHeart}>
-                {!isFilled ? (
-                    <svg className="w-[15px] h-[15px] mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="">
-                        <path d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z" />
-                    </svg>
-                ) : (
-                    <svg className="w-[15px] h-[15px] mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#EF4040">
-                        <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z" />
-                    </svg>
-                )}
-            </div>
-
-                   
-
+                    </div>  
                 </div>
+                <div className="absolute top-0 right-0 border border-transparent w-[40px] h-[40px] flex items-center justify-between rounded-md cursor-pointer py-2 bg-[#8DD3BB]" 
+                    onClick={toggleHeart}>
+                        {!isFilled ? (
+                            <svg className="w-[20px] h-[20px] mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#FFFFFF">
+                                <path d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z" />
+                            </svg>
+                        ) : (
+                            <svg className="w-[20px] h-[20px] mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#E61E2A">
+                                <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z" />
+                            </svg>
+                        )}
+                </div>  
+
+
             </div>
         </>
     )
