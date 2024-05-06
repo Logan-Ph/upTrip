55585f289b2961ea3652c50e23733f63829b2bf4
nguyenngocthanhmai <mainguyenngocthanh040426@gmail.com>
2024-05-06 23:31:58 +0700

    update

--- client/src/components/AdvancedExperienceCard.js
+++ client/src/components/AdvancedExperienceCard.js
@@ -59,8 +59,8 @@ function ExperienceCard({data}){
 
     return (
         <>
-        <div ref={ref}
-         className={`relative border border-[#CDEAE1] rounded-lg transition ease-in-out delay-100  hover:translate-x-1 hover:scale-105 duration-300 shadow-md ${visibilityClass}`}>
+            <div ref={ref}
+                className={`relative border border-[#CDEAE1] rounded-lg transition ease-in-out delay-100  hover:translate-x-1 hover:scale-105 duration-300 shadow-md ${visibilityClass}`}>
                 <div>
                     <img src={data?.content?.images?.[0]?.url}
                         className="w-full h-[245px] object-cover rounded-lg"
@@ -68,7 +68,7 @@ function ExperienceCard({data}){
                     />
                 </div>
 
-                <div className="px-4 py-6 space-y-2">
+                <div className="px-2 py-4 space-y-4">
                     {/* tours' name can be long (max 2 rows) */}
                     <p className="font-extrabold text-sm md:text-lg lg:text-xl line-clamp-2">{data?.content?.activity?.title}</p>
                     <div className="flex items-center">
@@ -84,46 +84,47 @@ function ExperienceCard({data}){
                                 />
                             </svg>
                         </div>
-                        {/* cái này m để nó theo cái tên mà ngta search á  */}
+                        
                         <div className="font-light text-sm md:text-md">
                             <span className="font-semibold text-sm md:text-md">{data?.content?.activity?.location?.city?.name}</span> {" | "} {data?.content?.activity?.location?.country?.name}
                         </div>
                     </div>
-                    <div className="flex items-center space-x-2">
-                        <div>
-                            <svg
-                                className="w-4 h-4 md:w-5 md:h-5"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 576 512"
-                            >
-                                <path
-                                    fill="#FFA732"
-                                    d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-                                />
-                            </svg>
-                        </div>
-                        <div className="border border-[#CDEAE1] px-1 md:px-2 bg-[#CDEAE1]">
-                            <p className="font-bold text-sm md:text-md">
-                                {data?.card?.commentInfo?.commentScore || data?.content?.reviewSummary?.averageScore}
-                                <span className="text-sm md:text-md font-light">
-                                    /5
-                                </span>
-                            </p>
-                        </div>
-                        <p className="text-sm font-light">{data?.content?.reviewSummary?.totalCount} reviews</p>
-                    </div>
 
-                    {/* Only tour hasthis section */}
-                    
-                        {data?.content?.badges?.activityHighlights?.map((badge, index) => (
-                            <>
-                            <div className="bg-[#CDEAE1] w-1/2 px-3" style={{ visibility: badge ? 'visible' : 'hidden' }}>
-                            <p className="font-semibold mx-auto">{badge?.badgeType.split("_").join(" ").toLowerCase()}</p>
-                        )}
+                    <div className="grid grid-cols-2">
+                        <div className="flex items-center space-x-1">
+                            <div>
+                                <svg
+                                    className="w-4 h-4 md:w-5 md:h-5"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 576 512"
+                                >
+                                    <path
+                                        fill="#FFA732"
+                                        d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+                                    />
+                                </svg>
                             </div>
-                            </>
-                           
-                    
+                            <div className="border border-[#CDEAE1] px-1 md:px-2 bg-[#CDEAE1]">
+                                <p className="font-bold text-sm md:text-md">
+                                    {data?.card?.commentInfo?.commentScore || data?.content?.reviewSummary?.averageScore}
+                                    <span className="text-sm md:text-md font-light">
+                                        /5
+                                    </span>
+                                </p>
+                            </div>
+                            <p className="text-sm font-light">{data?.content?.reviewSummary?.totalCount} reviews</p>
+                        </div>
+
+                        {data?.content?.badges?.activityHighlights?.length > 0 && (
+                            <div className="bg-[#CDEAE1] px-2 mx-auto">
+                                {data.content.badges.activityHighlights.map((badge, index) => (
+                                    <div key={index} className={badge ? 'visible' : 'hidden'}>
+                                        <p className="text-sm font-semibold mx-auto">{badge?.badgeType.split("_").join(" ").toLowerCase()}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                 </div>
                 <div className="flex flex-col items-end px-4 pb-4">
                     <div className="flex items-center">
