ba9a28c972b5a78d405db9d00604c5587448b68d
Logan <phamphuocsang5850@gmail.com>
2024-05-18 01:17:21 +0700

    change the headers config

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -801,9 +801,24 @@ exports.advancedSearchHotelBooking = async (req, res) => {
         );
 
         const headers = {
+            "Sec-Ch-Ua":
+                '"Microsoft Edge";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
             "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "Upgrade-Insecure-Requests": 1,
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+            Accept:
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "en-US,en;q=0.9,vi;q=0.8",
+            "Cache-Control": "max-age=0",
+            Dnt: "1",
+            Priority: "u=0, i"
         };
 
         const response = await axios.get(bookingAdvancedSearchHotelURL, {
